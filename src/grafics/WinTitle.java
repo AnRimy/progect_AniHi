ac673c84d055040691a4a9bd0541c01e3fa58d69
AnRimy <ardk03@bk.ru>
2025-09-11 16:06:39 +0300

    genres, name, desc in winTitle

--- src/grafics/WinTitle.java
+++ src/grafics/WinTitle.java
@@ -1,5 +1,7 @@
 package grafics;
 
+import java.util.ArrayList;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
 import javafx.scene.Scene;
@@ -16,8 +18,24 @@ public class WinTitle extends Stage{
 		BorderPane root = new BorderPane();
 		root.setStyle("fx-background-color: black");
 		
+		
 		ImageView img = new ImageView(image);
+		Label label_name = new Label(name);
+		Label label_desc = new Label(desc);
+		String genres = "";
+		JsonNode nodeGenres = data.path("genres");
+		if (nodeGenres.isArray()) {
+			for(JsonNode genre: nodeGenres)
+				genres+=(genre.path("title").asText("None")) + " ";
+		}
+		Label label_genres = new Label(genres);
+		System.out.println(data.path("genres"));
+		label_genres.setStyle("-fx-background-color: red;");
+		
 		root.setLeft(img);
+		root.setRight(label_name);
+		root.setCenter(label_desc);
+		root.setTop(label_genres);
 		
 		
 		
